ba10ebaaffcc500068ffd967b9a46f277205106a
tangdavid <tang.david.2000@gmail.com>
2020-06-30 16:12:29 -0700

    with job arrays

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -1,3 +1,8 @@
+#$ -N pheno-sim
+#$ -cwd
+#$ -t 1-9:1
+#$ -l h_rt=00:05:00,h_data=8G
+
 #!/bin/bash
 
 . /u/local/Modules/default/init/modules.sh
@@ -11,11 +16,21 @@ hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.geno
 pop="euro"
 out="../data/$pop/$pop"
 
+# SGE_TASK_ID=1
+
+h2=0.${SGE_TASK_ID}
+echo $heritibility
+
 # select european individuals
 grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $out.txt
 
 # filter vcf by MAF and pop
-# vcftools --gzvcf $hapmatrix --plink --maf 0.01 --keep $pop.txt --out $pop
+if [ -f ${out}.recode.vcf ]; then
+	echo "using filtered file: " ${out}.recode.vcf
+else 
+	echo "creating filtered file"
+	vcftools --gzvcf $hapmatrix --plink --maf 0.01 --keep $pop.txt --out $pop
+fi
 
 # create bed files for gcta sim
 plink --vcf $out.recode.vcf --make-bed --out $out
@@ -24,7 +39,8 @@ plink --vcf $out.recode.vcf --make-bed --out $out
 awk '{print $2}' $out.bim | sort | uniq > causal.snplist
 
 # Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
-$gcta --bfile $out --simu-qt --chr 1 --simu-hsq 0.5 --simu-rep 10 --simu-causal-loci causal.snplist --out $out
+$gcta --bfile $out --simu-qt --chr 1 --simu-hsq $h2 --simu-rep 10 --simu-causal-loci causal.snplist --out $out-h2-${h2}
 
-
+echo "sleeping"
+sleep 5m
 
