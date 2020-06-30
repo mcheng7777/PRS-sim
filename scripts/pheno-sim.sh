3c33a5291e6c6b96b5bdd1fa24799619e34a86bd
tangdavid <tang.david.2000@gmail.com>
2020-06-30 15:25:28 -0700

    newer gcta script

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -5,6 +5,7 @@ module load vcftools
 module load gcta
 module load plink
 
+gcta='../bin/gcta64'
 popinfo='../data/1000Genomes_Samples_Populations.txt'
 hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
 pop="euro"
@@ -19,9 +20,11 @@ grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $out.txt
 # create bed files for gcta sim
 plink --vcf $out.recode.vcf --make-bed --out $out
 
-awk '{print $2"\t"1}' $out.bim > causal.snplist
+# get causal traits
+awk '{print $2}' $out.bim | sort | uniq > causal.snplist
+
 # Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
-gcta64  --bfile $out --simu-qt --simu-hsq 0.5 --simu-rep 10 --simu-causal-loci causal.snplist --out $out
+$gcta --bfile $out --simu-qt --chr 1 --simu-hsq 0.5 --simu-rep 10 --simu-causal-loci causal.snplist --out $out
 
 
 
