0eddcbe5a4c0a2407cabf7e4888a3654ab18ed74
tangdavid <tang.david.2000@gmail.com>
2020-06-30 11:40:57 -0700

    kinda works but only simulates one snp

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -8,18 +8,20 @@ module load plink
 popinfo='../data/1000Genomes_Samples_Populations.txt'
 hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
 pop="euro"
+out="../data/$pop/$pop"
 
 # select european individuals
-grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $pop.txt
+grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $out.txt
 
 # filter vcf by MAF and pop
- vcftools --gzvcf $hapmatrix --plink --maf 0.01 --keep $pop.txt --out $pop
+# vcftools --gzvcf $hapmatrix --plink --maf 0.01 --keep $pop.txt --out $pop
 
 # create bed files for gcta sim
-plink --vcf $pop.recode.vcf --make-bed --out euro
+plink --vcf $out.recode.vcf --make-bed --out $out
 
-# Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 3 times
-# gcta64  --bfile test  --simu-qt  --simu-causal-loci causal.snplist  --simu-hsq 0.5 --simu-rep 10 --keep test.indi.list --out test
+awk '{print $2"\t"1}' $out.bim > causal.snplist
+# Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
+gcta64  --bfile $out --simu-qt --simu-hsq 0.5 --simu-rep 10 --simu-causal-loci causal.snplist --out $out
 
 
 
