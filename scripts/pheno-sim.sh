777852d0c5107868ec61a3ee7aabc9473ac8f19b
tangdavid <tang.david.2000@gmail.com>
2020-06-30 09:57:17 -0700

    combining scriptsw

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -2,6 +2,8 @@
 
 . /u/local/Modules/default/init/modules.sh
 module load vcftools
+module load gcta
+module load plink
 
 popinfo='../data/1000Genomes_Samples_Populations.txt'
 hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
@@ -9,6 +11,6 @@ pop="euro"
 
 grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $pop.txt
 
-vcftools --gzvcf $hapmatrix --recode --maf 0.01 --keep $pop.txt --out $pop-only
+vcftools --gzvcf $hapmatrix --plink --maf 0.01 --keep $pop.txt --out $pop-only
 
 
