64142b544a6c399f2717a347669ee08daeb342e0
tangdavid <tang.david.2000@gmail.com>
2020-07-01 11:54:10 -0700

    splitting data into training and validation

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -2,8 +2,9 @@
 #$ -cwd
 #$ -t 1-9:1
 #$ -l h_rt=00:05:00,h_data=8G
-
 #!/bin/bash
+
+# SGE_TASK_ID=1
 
 . /u/local/Modules/default/init/modules.sh
 module load vcftools
@@ -16,10 +17,9 @@ hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.geno
 pop="euro"
 out="../data/$pop/$pop"
 
-# SGE_TASK_ID=1
-
+# set heritibility parameter
 h2=0.${SGE_TASK_ID}
-echo $heritibility
+phenout=${out}-h2-${h2}
 
 # select european individuals
 grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $out.txt
@@ -39,8 +39,18 @@ plink --vcf $out.recode.vcf --make-bed --out $out
 awk '{print $2}' $out.bim | sort | uniq > causal.snplist
 
 # Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
-$gcta --bfile $out --simu-qt --chr 1 --simu-hsq $h2 --simu-rep 10 --simu-causal-loci causal.snplist --out $out-h2-${h2}
+$gcta --bfile $out --simu-qt --chr 1 --simu-hsq $h2 --simu-rep 10 --simu-causal-loci causal.snplist --out $phenout
 
+# training and validation file paths
+train=${phenout}-train.phen
+val=${phenout}-val.phen
+
+# splitting data into test and validation
+awk '{if(rand() < 0.2) {print $0 > "val"} else {print $0 > "train"}}' ${phenout}.phen
+mv val $val
+mv train $train
+
+# for hoffman time out
 echo "sleeping"
 sleep 5m
 
