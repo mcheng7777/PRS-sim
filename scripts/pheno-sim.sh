20ab4fa2c5ee23a156059b6aef250b6d30381378
tangdavid <tang.david.2000@gmail.com>
2020-07-06 12:25:07 -0700

    filtering out duplicate rsID's

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -8,6 +8,7 @@
 
 . /u/local/Modules/default/init/modules.sh
 module load vcftools
+module load bcftools
 module load gcta
 module load plink
 
@@ -15,7 +16,7 @@ gcta='../bin/gcta64'
 popinfo='../data/1000Genomes_Samples_Populations.txt'
 hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
 pop="euro"
-out="../data/$pop/$pop"
+out="../data/$pop/pheno/$pop"
 
 # set heritibility parameter
 h2=0.${SGE_TASK_ID}
@@ -30,6 +31,8 @@ if [ -f ${out}.recode.vcf ]; then
 else 
 	echo "creating filtered file"
 	vcftools --gzvcf $hapmatrix --plink --maf 0.01 --keep $pop.txt --out $pop
+	bcftools view ${out}.recode.vcf -Oz -o ${out}.vcf.gz
+	bcftools index ${out}.vcf.gz
 fi
 
 # create bed files for gcta sim
@@ -37,19 +40,24 @@ if [ -f ${out}.bed ]; then
 	echo "using files: " ${out}.bed", "${out}.fam", "${out}.bim
 else 
 	echo "creating *.bed, *.fam, *.bim files"
-	plink --vcf $out.recode.vcf --make-bed --out $out
+	plink --vcf ${out}.recode.vcf --make-bed --out ${out}-temp
+
+	# remove duplicates
+	cut -f 2 ${out}-temp.bim | sort | uniq -d > ${out}.dups
+	plink --bfile ${out}-temp --exclude ${out}.dups --make-bed --out ${out}
+	rm ${out}-temp*
 fi
 
 # get causal traits
-if [ -f causal.snplist ]; then
+if [ -f ${out}-causal.snplist ]; then
 	echo "using existing causal.snplist"
 else 
 	echo "generating new causal.snplist"
-	awk '{print $2}' $out.bim | sort | uniq > causal.snplist
+	awk '{print $2}' $out.bim > ${out}-causal.snplist
 fi
 
 # Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
-$gcta --bfile $out --simu-qt --chr 1 --simu-hsq $h2 --simu-rep 10 --simu-causal-loci causal.snplist --out $phenout
+$gcta --bfile $out --simu-qt --chr 1 --simu-hsq $h2 --simu-rep 100 --simu-causal-loci ${out}-causal.snplist --out $phenout
 
 # training and validation file paths
 train=${phenout}-train.phen
