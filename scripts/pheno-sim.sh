cda262a6ee3f798fe67cafcc927dce760b9dc1f4
tangdavid <tang.david.2000@gmail.com>
2020-07-06 14:24:53 -0700

    fixed some concurrency things

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -1,10 +1,7 @@
 #$ -N pheno-sim
 #$ -cwd
-#$ -t 1-9:1
-#$ -l h_rt=00:05:00,h_data=8G
+#$ -l h_rt=00:30:00,h_data=8G
 #!/bin/bash
-
-# SGE_TASK_ID=1
 
 . /u/local/Modules/default/init/modules.sh
 module load vcftools
@@ -17,10 +14,6 @@ popinfo='../data/1000Genomes_Samples_Populations.txt'
 hapmatrix='../data/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
 pop="euro"
 out="../data/$pop/pheno/$pop"
-
-# set heritibility parameter
-h2=0.${SGE_TASK_ID}
-phenout=${out}-h2-${h2}
 
 # select european individuals
 grep "CEU\|TSI\|FIN\|GBR\|IBS" $popinfo | awk '{print $1}' > $out.txt
@@ -57,16 +50,27 @@ else
 fi
 
 # Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
-$gcta --bfile $out --simu-qt --chr 1 --simu-hsq $h2 --simu-rep 100 --simu-causal-loci ${out}-causal.snplist --out $phenout
+
+for h2 in {1..9}
+do
+	$gcta \
+	--bfile $out \
+	--simu-qt \
+	--chr 1 \
+	--simu-hsq 0.${h2} \
+	--simu-rep 100 \
+	--simu-causal-loci ${out}-causal.snplist \
+	--out ${out}-h2-${h2}
+done
 
 # training and validation file paths
-train=${phenout}-train.phen
-val=${phenout}-val.phen
+# train=${phenout}-train.phen
+# val=${phenout}-val.phen
 
 # splitting data into test and validation
-sort -R ${phenout}.phen | awk '{if(rand() < 0.2) {print $0 > "val"} else {print $0 > "train"}}'
-mv train $train
-mv val $val
+# sort -R ${phenout}.phen | awk '{if(rand() < 0.2) {print $0 > "val"} else {print $0 > "train"}}'
+# mv train $train
+# mv val $val
 
 # for hoffman time out
 echo "sleeping"
