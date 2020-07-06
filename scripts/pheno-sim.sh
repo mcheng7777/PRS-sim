b815a6f48e7f77a993a2f4080b301e2616ff4a10
tangdavid <tang.david.2000@gmail.com>
2020-07-06 16:51:04 -0700

    incorporate standardization

--- scripts/pheno-sim.sh
+++ scripts/pheno-sim.sh
@@ -8,6 +8,7 @@ module load vcftools
 module load bcftools
 module load gcta
 module load plink
+module load R/3.5.1
 
 gcta='../bin/gcta64'
 popinfo='../data/1000Genomes_Samples_Populations.txt'
@@ -49,7 +50,7 @@ else
 	awk '{print $2}' $out.bim > ${out}-causal.snplist
 fi
 
-# Simulate a quantitative trait with the heritability of 0.5 for a subset of individuals for 1 times
+# simulate a quantitative traits at various heritability levels
 
 for h2 in {1..9}
 do
@@ -60,9 +61,11 @@ do
 	--simu-hsq 0.${h2} \
 	--simu-rep 100 \
 	--simu-causal-loci ${out}-causal.snplist \
-
 	--out ${out}-h2-${h2}
 done
+
+# standardized the phenotypes
+Rscript ./standardize_phen.R /u/project/sriram/dtang200/PRS-sim/data/euro/pheno
 
 # split into training and validation
 awk '{print $2}' ${out}.fam | sort -R > indi-rand.txt
