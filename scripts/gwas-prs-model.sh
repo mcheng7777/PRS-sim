a6c7d8765eb0cb195d4a045f43dc8266c0668228
Michael Cheng <mikechen@n2006.(none)>
2020-07-06 16:00:02 -0700

    hide covariates

--- scripts/gwas-prs-model.sh
+++ scripts/gwas-prs-model.sh
@@ -12,7 +12,7 @@ module load plink
 
 # create directory and file variables
 bin_files=../data/euro/euro
-phen_file=../data/euro/euro-h2-0.1-train.phen
+phen_file=../data/euro/euro-h2/euro-h2-0.1.phen.scaled
 outdir=../data/euro/gwas/gwas-pca-covar
 pca_out=../data/euro/pca
 mkdir $outdir
@@ -22,7 +22,7 @@ mkdir $pca_out
 # run pca
 # plink --bfile $bin_files --pca 5 'header' --out ${pca_out}/h2-0.1-pca
 # run plink quantitative association simulation
-plink --bfile $bin_files --linear --pheno $phen_file --all-pheno --allow-no-sex --covar ${pca_out}/h2-0.1-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 --out ${outdir}/h2-0.1
+plink --bfile $bin_files --linear 'hide-covar' --pheno $phen_file --all-pheno --allow-no-sex --covar ${pca_out}/h2-0.1-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 --out ${outdir}/h2-0.1-scaled
 
 
 # for hoffman time requirement
