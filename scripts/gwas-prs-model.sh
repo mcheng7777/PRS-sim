bcc681d2a27ec88bf0f515e5df2f3d65c6ed897f
Michael Cheng <mikechen@n7240.(none)>
2020-07-08 16:40:36 -0700

    run on genotype simulation

--- scripts/gwas-prs-model.sh
+++ scripts/gwas-prs-model.sh
@@ -11,40 +11,30 @@
 module load plink
 
 # create directory and file variables
-<<<<<<< HEAD
-<<<<<<< HEAD
 pop="sim"
 bin_files="../data/${pop}/pheno/${pop}"
-phen_file="../data/${pop}/pheno-test/${pop}-h2-1-scaled-train.phen"
-outdir="../data/${pop}/gwas/gwas-pca-covar"
-pca_out="../data/${pop}/pca"
-=======
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-bin_files="../data/euro/pheno/euro"
-phen_file="../data/euro/pheno/euro-h2-1-scaled-train.phen"
-outdir="../data/euro/gwas/gwas-pca-covar"
-pca_out="../data/euro/pca"
-<<<<<<< HEAD
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
+phen_file="../data/${pop}/pheno-test/${pop}-h2-4-scaled-train.phen"
+outdir="../data/${pop}/gwas/"
+pca_out="../data/${pop}/pca/"
+herit="h2-0.4"
+
 mkdir $outdir
 
 mkdir $pca_out
 
 #LD prune and PCA
 plink \
-        --bfile $b_files \
+        --bfile $bin_files \
         --indep-pairwise 200 50 0.25 \
-        --out "../data/${pop}/pca/${outname}"
+        --out ${pca_out}${herit}
 plink \
-        --bfile $b_files \
-        --extract "../data/${pop}/pca/${outname}.prune.in" \
+        --bfile $bin_files \
+        --extract ${pca_out}${herit}.prune.in \
         --pca 5 \
-        --out "../data/${pop}/pca/${outname}-pruned-pca"\
+        --out ${pca_out}${herit}-pruned-pca
+
 # run plink quantitative association simulation
-plink --bfile $bin_files --linear 'hide-covar' --pheno $phen_file --all-pheno --allow-no-sex --covar ${pca_out}/h2-0.1-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 --out ${outdir}/h2-0.1-scaled
+plink --bfile $bin_files --linear 'hide-covar' --pheno $phen_file --all-pheno --allow-no-sex --covar ${pca_out}${herit}-pruned-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 --out ${outdir}${herit}
 
 
 # for hoffman time requirement
