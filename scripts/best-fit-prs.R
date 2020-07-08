655c8d0eca6b5073d2f7ce41c7881e927c56b8f6
Michael Cheng <mikechen@n7240.(none)>
2020-07-08 16:20:25 -0700

    fix chaos

--- scripts/best-fit-prs.R
+++ scripts/best-fit-prs.R
@@ -1,31 +1,20 @@
 #!/u/local/apps/R/3.5.1/gcc-4.9.3_MKL-2018/bin/R
-<<<<<<< HEAD
-<<<<<<< HEAD
 args = commandArgs(trailingOnly = TRUE)
 
 # 1 argument - desired directory
 if (length(args)!=2) {
   stop("2 arguments must be supplied: h2-0.1-train /abspath/to/phenotype/file", call.=FALSE)
 }
-=======
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
 # args = commandArgs(trailingOnly = TRUE)
 
 # # 1 argument - desired directory
 # if (length(args)!=1) {
 #   stop("1 arguments must be supplied: /abspath/to/directory/", call.=FALSE)
 # }
-<<<<<<< HEAD
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
 
 
 setwd("/u/home/m/mikechen/project-sriram/PRS-sim/data/euro/prs")
 p.threshold <- c(0.001,0.05,0.1,0.2,0.3,0.4,0.5)
-<<<<<<< HEAD
-<<<<<<< HEAD
 outname = paste0(args[1],".")
 phen_file <- args[2]
 # Read in the phenotype file 
@@ -36,20 +25,7 @@ colnames(phenotype) <- c("FID","IID","Phen")
 # Read in the PCs
 pcs <- read.table("/u/home/m/mikechen/project-sriram/PRS-sim/data/euro/pca/h2-0.1-pruned-pca.eigenvec", header=F)
 colnames(pcs) <- c("FID", "IID", paste0("PC",1:5))
-=======
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-outname <- "h2-0.1."
-# Read in the phenotype file 
-phenotype <- read.table("/u/home/m/mikechen/project-sriram/PRS-sim/data/euro/pheno-test/euro-h2-1-scaled-train.phen", header=F)
-phenotype <- phenotype[,c(1:3)]
-colnames(phenotype) <- c("FID","IID","Phen")
-# Read in the PCs
-pcs <- read.table("/u/home/m/mikechen/project-sriram/PRS-sim/data/euro/pca/h2-0.1-pca.eigenvec", header=T)
-<<<<<<< HEAD
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
+
 
 # no covariates
 # Read in the covariates (here, it is sex)
@@ -87,15 +63,8 @@ for(i in p.threshold){
 }
 # Best result is:
 prs.result[which.max(prs.result$R2),]
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 maxthresh <- prs.result[which.max(prs.result$R2),]$Threshold[1]
-=======
-
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-=======
-
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
 
 # Plot PRS
 
@@ -144,8 +113,6 @@ ggplot(data = prs.result, aes(x = factor(Threshold), y = R2)) +
                                  1)
   )
 # save the plot
-<<<<<<< HEAD
-<<<<<<< HEAD
 ggsave(paste0(outname,"png"), height = 7, width = 7)
 
 
@@ -153,45 +120,13 @@ ggsave(paste0(outname,"png"), height = 7, width = 7)
 prs <- read.table(paste0(outname,maxthresh,".profile"), header=T)
 # Merge the files
 dat <- merge(prs, phenotype)
-=======
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-ggsave(paste0(outname,".png"), height = 7, width = 7)
-q() # exit R
 
 
-# Read in the files
-prs <- read.table(paste0(outname,"0.4.profile"), header=T)
-phen_val <- read.table("/u/home/m/mikechen/project-sriram/PRS-sim/data/euro/pheno-test/euro-h2-1-scaled-val.phen", header=F)
-phen_val <- phen_val[,c(1:3)]
-colnames(phen_val) <- c("FID","IID","Phen")
-# Merge the files
-dat <- merge(prs, phenotype)
-dat_val <- merge(prs, phen_val)
-<<<<<<< HEAD
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
 # Start plotting
 ggplot(dat, aes(x=SCORE, y=Phen))+
   geom_point()+
   theme_classic()+
   labs(x="Polygenic Score", y="Phen")
-<<<<<<< HEAD
-<<<<<<< HEAD
-ggsave(paste0(outname,maxthresh,"-PRS-plot.png"), height = 7, width = 7)
-=======
-=======
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-ggsave(paste0(outname,"0.4-PRS-plot.png"), height = 7, width = 7)
 
-ggplot(dat_val, aes(x=SCORE, y=Phen))+
-  geom_point()+
-  theme_classic()+
-  labs(x="Polygenic Score", y="Phen")
-<<<<<<< HEAD
-ggsave(paste0(outname,"0.4-PRS-plot-val.png"), height = 7, width = 7)
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
-=======
-ggsave(paste0(outname,"0.4-PRS-plot-val.png"), height = 7, width = 7)
->>>>>>> a0d71329980097c4168c31a5d32902a8997905e0
+ggsave(paste0(outname,maxthresh,"-PRS-plot.png"), height = 7, width = 7)
+
