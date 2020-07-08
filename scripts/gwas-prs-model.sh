2b8ff7b84cdd1de21244b43ebf66b8a7d9b18d22
Michael Cheng <mikechen@n7240.(none)>
2020-07-08 15:04:43 -0700

    for genotype sim

--- scripts/gwas-prs-model.sh
+++ scripts/gwas-prs-model.sh
@@ -11,20 +11,29 @@
 module load plink
 
 # create directory and file variables
-bin_files="../data/euro/pheno/euro"
-phen_file="../data/euro/pheno/euro-h2-1-scaled-train.phen"
-outdir="../data/euro/gwas/gwas-pca-covar"
-pca_out="../data/euro/pca"
+pop="sim"
+bin_files="../data/${pop}/pheno/${pop}"
+phen_file="../data/${pop}/pheno-test/${pop}-h2-1-scaled-train.phen"
+outdir="../data/${pop}/gwas/gwas-pca-covar"
+pca_out="../data/${pop}/pca"
 mkdir $outdir
 
 mkdir $pca_out
 
-# run pca
-# plink --bfile $bin_files --pca 5 'header' --out ${pca_out}/h2-0.1-pca
+#LD prune and PCA
+plink \
+        --bfile $b_files \
+        --indep-pairwise 200 50 0.25 \
+        --out "../data/${pop}/pca/${outname}"
+plink \
+        --bfile $b_files \
+        --extract "../data/${pop}/pca/${outname}.prune.in" \
+        --pca 5 \
+        --out "../data/${pop}/pca/${outname}-pruned-pca"\
 # run plink quantitative association simulation
 plink --bfile $bin_files --linear 'hide-covar' --pheno $phen_file --all-pheno --allow-no-sex --covar ${pca_out}/h2-0.1-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 --out ${outdir}/h2-0.1-scaled
 
 
 # for hoffman time requirement
-# echo "sleeping"
-# sleep 5m
+echo "sleeping"
+sleep 5m
