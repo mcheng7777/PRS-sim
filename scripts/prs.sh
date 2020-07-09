6429dc45d9985aa6390fd0a818d8a72140dd88e7
Michael Cheng <mikechen@n7240.(none)>
2020-07-08 17:47:06 -0700

    run genotype simulation

--- scripts/prs.sh
+++ scripts/prs.sh
@@ -15,19 +15,20 @@ module load plink
 # phenotype matrix
 # covariate matrix
 
-sum_stats="../data/euro/gwas-test/h2-0.1-scaled.P1.assoc.linear"
-b_files="../data/euro/pheno-test/euro"
-phen_file="../data/euro/pheno-test/euro-h2-1-scaled-train.phen"
-phen_val="../data/euro/pheno-test/euro-h2-1-scaled-val.phen"
-cov_file="../data/euro/pca/h2-0.1-pca.eigenvec"
+pop="sim"
+herit="h2-0.4"
+sum_stats="../data/${pop}/gwas/${herit}.P1.assoc.linear"
+b_files="../data/${pop}/pheno/${pop}"
+phen_file="../data/${pop}/pheno-test/${pop}-h2-4-scaled-train.phen"
+phen_val="../data/${pop}/pheno-test/${pop}-h2-4-scaled-val.phen"
 
-outdir="../data/euro/prs/"
+outdir="../data/${pop}/prs/"
 mkdir $outdir
-outname="h2-0.1"
+
 # step 1 - clumping/LD
-if [ -f ${outdir}${outname}.clumped ] 
+if [ -f ${outdir}${herit}.clumped ] 
 then
-	echo "using ${outdir}${outname}.clumped"
+	echo "using ${outdir}${herit}.clumped"
 else
 	plink \
 		--bfile $b_files \
@@ -37,52 +38,56 @@ else
 		--clump $sum_stats \
 		--clump-snp-field SNP \
 		--clump-field P \
-		--out ${outdir}${outname}
+		--out ${outdir}${herit}
 fi
 
 # extract SNP ids
-awk 'NR!=1{print $3}' ${outdir}${outname}.clumped > ${outdir}${outname}.valid.snp
+awk 'NR!=1{print $3}' ${outdir}${herit}.clumped > ${outdir}${herit}.valid.snp
 
 # snp id's and p-values
-awk '{print $2,$9}' $sum_stats > ${outdir}${outname}.SNP.pvalue
+awk '{print $2,$9}' $sum_stats > ${outdir}${herit}.SNP.pvalue
 
 # pvalue range list for inclusion in PRS
-echo "0.001 0 0.001" > ${outdir}${outname}.rangelist
-echo "0.05 0 0.05" >> ${outdir}${outname}.rangelist
-echo "0.1 0 0.1" >> ${outdir}${outname}.rangelist
-echo "0.2 0 0.2" >> ${outdir}${outname}.rangelist
-echo "0.3 0 0.3" >> ${outdir}${outname}.rangelist
-echo "0.4 0 0.4" >> ${outdir}${outname}.rangelist
-echo "0.5 0 0.5" >> ${outdir}${outname}.rangelist
+echo "0.001 0 0.001" > ${outdir}${herit}.rangelist
+echo "0.05 0 0.05" >> ${outdir}${herit}.rangelist
+echo "0.1 0 0.1" >> ${outdir}${herit}.rangelist
+echo "0.2 0 0.2" >> ${outdir}${herit}.rangelist
+echo "0.3 0 0.3" >> ${outdir}${herit}.rangelist
+echo "0.4 0 0.4" >> ${outdir}${herit}.rangelist
+echo "0.5 0 0.5" >> ${outdir}${herit}.rangelist
 
 # calculate PRS for training and validation
 plink \
 	--bfile $b_files \
 	--pheno $phen_file --all-pheno --allow-no-sex \
 	--score $sum_stats 2 4 7 header \
-	--q-score-range ${outdir}${outname}.rangelist ${outdir}${outname}.SNP.pvalue \
-	--extract ${outdir}${outname}.valid.snp \
-	--out ${outdir}${outname}-train
+	--q-score-range ${outdir}${herit}.rangelist ${outdir}${herit}.SNP.pvalue \
+	--extract ${outdir}${herit}.valid.snp \
+	--out ${outdir}${herit}-train
 
 plink \
         --bfile $b_files \
         --pheno $phen_val --all-pheno --allow-no-sex \
         --score $sum_stats 2 4 7 header \
-        --q-score-range ${outdir}${outname}.rangelist ${outdir}${outname}.SNP.pvalue \
-        --extract ${outdir}${outname}.valid.snp \
-        --out ${outdir}${outname}-val
+        --q-score-range ${outdir}${herit}.rangelist ${outdir}${herit}.SNP.pvalue \
+        --extract ${outdir}${herit}.valid.snp \
+        --out ${outdir}${herit}-val
 
 #LD prune and PRS
-plink \
-	--bfile $b_files \
-	--indep-pairwise 200 50 0.25 \
-	--out "../data/euro/pca/${outname}"
-plink \
-	--bfile $b_files \
-	--extract "../data/euro/pca/${outname}.prune.in" \
-	--pca 5 \
-	--out "../data/euro/pca/${outname}-pruned-pca"
-
+if [ -f ../data/${pop}/pca/${herit}.prune.in ]
+then
+	echo "using ../data/${pop}/pca/${herit}.prune.in"
+else
+	plink \
+		--bfile $b_files \
+		--indep-pairwise 200 50 0.25 \
+		--out "../data/${pop}/pca/${herit}"
+	plink \
+		--bfile $b_files \
+		--extract "../data/${pop}/pca/${herit}.prune.in" \
+		--pca 5 header \
+		--out "../data/${pop}/pca/${herit}-pruned-pca"
+fi
 
 echo "sleeping"
 sleep 5m
