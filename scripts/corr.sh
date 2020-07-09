31073c0fb3143623a788f44eabe5afee9ee64ed5
tangdavid <tang.david.2000@gmail.com>
2020-07-08 17:20:55 -0700

    load R

--- scripts/corr.sh
+++ scripts/corr.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#$ -N prs-sim
+#$ -N val-corr
 #$ -l h_rt=00:30:00,h_data=8G
 #$ -t 1-10:1 
 #$ -cwd
 
 #!/bin/bash
+
+. /u/local/Modules/default/init/modules.sh
+module load R/3.5.1
 
 h2=$(( SGE_TASK_ID - 1))
 pop="sim"
@@ -22,3 +25,5 @@ done
 echo "sleeping"
 sleep 5m
 echo "done sleeping"
+
+
