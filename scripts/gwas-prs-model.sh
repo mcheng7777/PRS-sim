59aaee3d42098fb4597e0b946bb0b4390988cd2a
Michael Cheng <mikechen@n7200.(none)>
2020-07-09 18:26:17 -0700

    iterate all h2

--- scripts/gwas-prs-model.sh
+++ scripts/gwas-prs-model.sh
@@ -1,10 +1,9 @@
 
 #$ -N gwas-sim
 #$ -cwd
-#$ -l h_rt=1:00:00,h_data=8G
+#$ -l h_rt=5:00:00,h_data=8G
 
 #!/bin/bash
-
 
 # load modules
 . /u/local/Modules/default/init/modules.sh
@@ -13,29 +12,33 @@ module load plink
 # create directory and file variables
 pop="sim"
 bin_files="../data/${pop}/pheno/${pop}"
-phen_file="../data/${pop}/pheno-test/${pop}-h2-4-scaled-train.phen"
 outdir="../data/${pop}/gwas/"
 pca_out="../data/${pop}/pca/"
-herit="h2-0.4"
-
 mkdir $outdir
-
 mkdir $pca_out
 
-#LD prune and PCA
-plink \
-        --bfile $bin_files \
-        --indep-pairwise 200 50 0.25 \
-        --out ${pca_out}${herit}
-plink \
-        --bfile $bin_files \
-        --extract ${pca_out}${herit}.prune.in \
-        --pca 5 header \
-        --out ${pca_out}${herit}-pruned-pca
-
-# run plink quantitative association simulation
-plink --bfile $bin_files --linear 'hide-covar' --pheno $phen_file --all-pheno --allow-no-sex --covar ${pca_out}${herit}-pruned-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 --out ${outdir}${herit}
-
+for h in {0..9}
+do
+	herit="h2-${h}"
+	phen_file="../data/${pop}/pheno-test/${pop}-${herit}-scaled-train.phen"
+	#LD prune and PCA
+	plink \
+		--bfile $bin_files \
+		--indep-pairwise 200 50 0.25 \
+		--out ${pca_out}${herit}
+	plink \
+		--bfile $bin_files \
+		--extract ${pca_out}${herit}.prune.in \
+		--pca 5 header \
+		--out ${pca_out}${herit}-pruned-pca
+	# run plink quantitative association simulation
+	plink \
+		--bfile $bin_files \
+		--linear 'hide-covar' \
+		--pheno $phen_file --all-pheno --allow-no-sex \
+		--covar ${pca_out}${herit}-pruned-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 \
+		--out ${outdir}${herit}
+done
 
 # for hoffman time requirement
 echo "sleeping"
