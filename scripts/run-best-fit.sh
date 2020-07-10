ee8a77cd996c244a00c0d1f7abc48f3024f08305
Michael Cheng <mikechen@n7200.(none)>
2020-07-09 18:28:07 -0700

    iterate all h2 replicates and pvalues

--- scripts/run-best-fit.sh
+++ scripts/run-best-fit.sh
@@ -1,24 +1,32 @@
+#$ -N prs-plots
+#$ -l h_rt=1:00:00,h_data=8G
+#$ -t 1-10:1
+#$ -cwd
 
 #!/u/bin/bash
 
-
+ 
 # load modules
 . /u/local/Modules/default/init/modules.sh
 
 module load R/3.5.1
 
-train_phen="/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/pheno-test/sim-h2-4-scaled-train.phen"
-val_phen="/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/pheno-test/sim-h2-4-scaled-val.phen"
-herit="h2-0.4"
-prs_dir="/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/prs/"
-pca_file="/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/pca/h2-0.4-pruned-pca.eigenvec"
-
-echo "generating training plots"
-# plot training prs
-Rscript best-fit-prs.R ${herit}-train $train_phen $pca_file $prs_dir 
-
-echo "generating validation plots"
-# plot test prs
-Rscript best-fit-prs.R ${herit}-val $val_phen $pca_file $prs_dir
+# SGE_TASK_ID=10
+pop="sim"
+prs_dir="/u/home/m/mikechen/project-sriram/PRS-sim/data/${pop}/prs/"
+herit=$(( SGE_TASK_ID - 1))
+mkdir ${prs_dir}plots/
+echo $herit
+for r in {1..10}
+do    val_phen="/u/home/m/mikechen/project-sriram/PRS-sim/data/${pop}/pheno-test/${pop}-h2-${herit}-scaled-val.phen"
+    name=h2-${herit}.P${r}
+    pca_file="/u/home/m/mikechen/project-sriram/PRS-sim/data/${pop}/pca/h2-${herit}-pruned-pca.eigenvec"
 
 
+    echo "generating validation plots"
+    # plot test prs
+    Rscript best-fit-prs.R ${name} $pca_file $prs_dir
+done
+
+echo "sleeping"
+sleep 5m
