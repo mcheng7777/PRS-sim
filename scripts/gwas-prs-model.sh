e98a411023a2be8878a712aad812d5cb07314c9b
Michael Cheng <mikechen@n2192.(none)>
2020-07-13 18:14:46 -0700

    bug fixes

--- scripts/gwas-prs-model.sh
+++ scripts/gwas-prs-model.sh
@@ -15,27 +15,31 @@ bin_files="../data/${pop}/pheno/${pop}"
 outdir="../data/${pop}/gwas/"
 pca_out="../data/${pop}/pca/"
 
-
-for h in {0..9}
-do
-	herit="h2-${h}"
-	phen_file="../data/${pop}/pheno/${pop}-${herit}-scaled-train.phen"
+if [ -f ${pca_out}${pop}.prune.in ]
+then
+    echo "using ${pca_out}${pop}.prune.in"
+else
 	#LD prune and PCA
 	plink \
 		--bfile $bin_files \
 		--indep-pairwise 200 50 0.25 \
-		--out ${pca_out}${herit}
+		--out ${pca_out}${pop}
 	plink \
 		--bfile $bin_files \
-		--extract ${pca_out}${herit}.prune.in \
+		--extract ${pca_out}${pop}.prune.in \
 		--pca 5 header \
-		--out ${pca_out}${herit}-pruned-pca
+		--out ${pca_out}${pop}-pruned-pca
+fi
+for h in {0..9}
+do
+    herit="h2-${h}"
+    phen_file="../data/${pop}/pheno/${pop}-${herit}-scaled-train.phen"
 	# run plink quantitative association simulation
 	plink \
 		--bfile $bin_files \
 		--linear 'hide-covar' \
 		--pheno $phen_file --all-pheno --allow-no-sex \
-		--covar ${pca_out}${herit}-pruned-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 \
+		--covar ${pca_out}${pop}-pruned-pca.eigenvec --covar-name PC1, PC2, PC3, PC4, PC5 \
 		--out ${outdir}${herit}
 done
 
