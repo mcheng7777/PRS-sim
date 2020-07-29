2d190feab829648fbd99ece921735a2caec0ac59
Michael Cheng <mikechen@n7430.(none)>
2020-07-29 10:14:30 -0700

    update file paths

--- scripts/run-best-fit.sh
+++ scripts/run-best-fit.sh
@@ -1,16 +1,10 @@
 #$ -N job-prs-plots
-#$ -l h_rt=1:00:00,h_data=8G
+#$ -l h_rt=00:10:00,h_data=8G
 #$ -t 1-10:1
 #$ -cwd
 
 #!/u/bin/bash
 
-if [ $# -eq 0 ]
-then
-	echo "Usage: qsub run-best-fit.sh [population]"
-	sleep 5m
-	exit 1
-fi
  
 # load modules
 . /u/local/Modules/default/init/modules.sh
@@ -19,17 +13,28 @@ module load R/3.5.1
 
 # SGE_TASK_ID=10
 pop=$1
-working_dir="../data/${pop}"
+effect=$2
+working_dir="../data/train/${pop}"
 pca_file="pca/${pop}-pruned-pca.eigenvec"
 herit=$(( SGE_TASK_ID - 1))
-outfile="genetic-max-pvals-${herit}.txt"
+if [ $effect == "genetic" ]
+then
+	outfile="genetic-max-pvals-${herit}.txt"
+else
+	outfile="max-pvals-${herit}.txt"
+fi
 echo $herit
 for r in {1..100}
 do
 	name="${pop}-h2-${herit}-r-${r}"
 	echo "generating validation plots"
 	# plot test prs
-	Rscript best-fit-prs.R ${name} $pca_file $working_dir $outfile
+	if [ $effect = "genetic" ]
+	then
+		Rscript best-fit-genetic-prs.R ${name} $pca_file $working_dir $outfile
+	else
+		Rscript best-fit-prs.R ${name} $pca_file $working_dir $outfile
+	fi
 done
 
 echo "sleeping"
