d96bbb23973bfcaf8956a914071f59f9b83e88c2
Michael Cheng <mikechen@login2.(none)>
2020-08-11 15:54:44 -0700

    bug fixes

--- scripts/gwas-sim/train-val-test.sh
+++ scripts/gwas-sim/train-val-test.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
+## Description: This script partitions one population's phenotype data into training, validation, and testing
 
+# training and validation population
 pop=$1
-# testpop=$3
-effect=$2
+effect=$3
 outdir="../../data/train/$pop/pheno"
-# testdir="../../data/val/$testpop/pheno"
 names="${outdir}/${pop}-indi"
-# testnames="${testdir}/${testpop}-indi"
 out="${outdir}/$pop"
-# testout="${testdir}/$testpop"
-awk '{print $1}' ${out}.fam | sort -R > ${names}-rand.txt
-# awk '{print $1}' ${testout}.fam > ${testnames}-test.txt
-# awk -v a=${out}-afr.txt -v e=${out}-eur.txt '{ if ($1 ~ /afr/) print $1 > a; else print $1 > e; }' ${out}.fam
-total=$(cat ${names}-rand.txt | wc -l)
-train=$(( total / 100 * 80 ))
-# val=$(( total / 100 * 20))
 
-head -n $train ${names}-rand.txt > ${names}-train.txt
-tail -n +$((train + 1)) ${names}-rand.txt > ${names}-val.txt # | head -n ${val} > ${names}-val.txt
-# tail -n +$((train + $val + 1)) ${names}-rand.txt > ${names}-test.txt
+# testing population
+testpop=$2
+testdir="../../data/val/$testpop/pheno"
+testnames="${testdir}/${testpop}-indi"
+testout="${testdir}/$testpop"
+
+
 
 for h2 in {0..9}
 do
 	# Read In original train file and split into train and validation
 	# data="../../data/${pop}/pheno/${pop}-h2-${h2}-scaled"
-	data="../../data/train/${pop}/pheno/${pop}-h2-${h2}"
-	datagen="../../data/train/${pop}/pheno/${pop}-h2-${h2}-genetic"
+	data="${outdir}/${pop}-h2-${h2}"
+	testdata="${testdir}/${testpop}-h2-${h2}"
+	datagen="../../data/train/${pop}/blup/${pop}-h2-${h2}-genetic"
 	# echo "partitioning h2: ${h2}"
 	# grep -F -wf ${names}-train.txt ${data}.phen > ${data}-train.phen
 	# grep -F -wf ${names}-val.txt ${data}.phen > ${data}-val.phen
@@ -34,17 +31,27 @@ do
 	# If applicable, make sure genetic-effect file has same ID columns as original effect file 
 	if [ $effect == "genetic" ]
 	then
-		grep -F -wf ${names}-train.txt ${datagen}-train.phen > ${datagen}-train-temp.phen
-        	grep -F -wf ${names}-val.txt  ${datagen}-train.phen > ${datagen}-val-temp.phen
-		awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' ${data}-train.phen ${datagen}-train-temp.phen > ${datagen}-train.phen
-		awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' ${data}-val.phen ${datagen}-val-temp.phen > ${datagen}-val.phen
+		awk '{print $1}' ${data}-train.phen > ${names}-genetic-train.txt
+		awk '{print $1}' ${data}-val.phen > ${names}-genetic-val.txt
+		grep -F -wf ${names}-genetic-train.txt ${datagen}-train.phen > ${data}-train-temp.phen
+        	grep -F -wf ${names}-genetic-val.txt  ${datagen}-train.phen > ${data}-val-temp.phen
+		awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' ${data}-train.phen ${data}-train-temp.phen > ${data}-genetic-train.phen
+		awk 'FNR==NR{a[NR]=$2;next}{$2=a[FNR]}1' ${data}-val.phen ${data}-val-temp.phen > ${data}-genetic-val.phen
 		# cp ${data}-val.phen ${data}-scaled-test.phen
-		rm ${datagen}-train-temp.phen
-		rm ${datagen}-val-temp.phen
+		rm ${data}-train-temp.phen
+		rm ${data}-val-temp.phen
 	else
+		awk '{print $1}' ${out}.fam | sort -R > ${names}-rand.txt
+		# awk -v a=${out}-afr.txt -v e=${out}-eur.txt '{ if ($1 ~ /afr/) print $1 > a; else print $1 > e; }' ${out}.fam
+		total=$(cat ${names}-rand.txt | wc -l)
+		train=$(( total / 100 * 70 ))
+		val=$(( total / 100 * 20 ))
+		head -n $train ${names}-rand.txt > ${names}-train.txt
+		tail -n +$(($train + 1)) ${names}-rand.txt | head -n ${val} > ${names}-val.txt
+		tail -n +$(($train + $val + 1)) ${names}-rand.txt > ${testnames}-test.txt
 		grep -F -wf ${names}-train.txt ${data}.phen > ${data}-train.phen
         	grep -F -wf ${names}-val.txt  ${data}.phen > ${data}-val.phen
-		grep -F -wf ${names}-val.txt  ${data}.phen > ${data}-test.phen
+		grep -F -wf ${testnames}-test.txt  ${data}.phen > ${testdata}.phen
 		# cp ${data}-val.phen ${data}-scaled-test.phen
 	fi
 done
