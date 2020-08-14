71c1bc8b84fb53bb171acc22ed13d3b578841f98
Michael Cheng <mikechen@login3.(none)>
2020-08-13 22:59:28 -0700

    clear old script

--- scripts/gwas-prs/best-fit-prs.R
+++ scripts/gwas-prs/best-fit-prs.R
@@ -1,64 +1,13 @@
 # #!/u/local/apps/R/3.5.1/gcc-4.9.3_MKL-2018/bin/R
-# library(dplyr)
-# args = commandArgs(trailingOnly = TRUE)
-# if (length(args)!=3) {
-#   stop("3 arguments must be supplied: h2-0.P1 /abspath/to/pca.eigenvec /abspath/to/prs/directory", call.=FALSE)
-# }
-# setwd(paste0("../data/",args[1]))
-# 
-# corr_files <- grep('corr-[0-9]',list.files(),value=T)
-# maxp <- data.frame(matrix(ncol=4,nrow=-0))
-# colnames(maxp) <- c("heritability","replica","pval","R2")
-# row_i=1
-# for (c in corr_files){
-#   df <- read.table(c,header=T)
-#   df2 <- group_by(df,replica)%>%
-#     group_split()
-#   for (d in df2){
-#     m <- d[which.max(d$R2),]
-#     maxp[row_i,] <- m[1,]
-#     row_i <- row_i+1
-#   }
-# }
-# write.table(maxp,file="max-corr-with-pval.txt",sep='\t',quote=F,row.names=F)
-# write.table(maxp[,c(1,2,4)],file="max-corr.txt",sep='\t',quote=F,row.names=F)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 library(stringr)
 args = commandArgs(trailingOnly = TRUE)
-#
-# args[1] <- "h2-0.4-train"
-# args[2] <- "/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/pheno-test/sim-h2-4-scaled-train.phen"
-# args[3] <- "/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/pca/h2-0.4-pruned-pca.eigenvec"
-# args[4] <- "/u/home/m/mikechen/project-sriram/PRS-sim/data/sim/prs/"
 # 1 argument - desired directory
 print(args)
 
 if (length(args)!=4) {
   stop("4 arguments must be supplied: h2-0.P1 /abspath/to/pca.eigenvec /abspath/to/prs/directory outfile", call.=FALSE)
 }
-# args = commandArgs(trailingOnly = TRUE)
-
-# # 1 argument - desired directory
-# if (length(args)!=1) {
-#   stop("1 arguments must be supplied: /abspath/to/directory/", call.=FALSE)
-# }
 
 
 setwd(args[3])
@@ -117,76 +66,4 @@ maxthresh <- prs.result[which.max(prs.result$R2),]
 h2 <- str_split(outname,"-")[[1]][3]
 r <- str_sub(str_split(outname,"-")[[1]][5],1,-2)
 write(paste0(h2,"\t",r,"\t",maxthresh$Threshold,"\t",maxthresh$R2),file=args[4],append=T)
-# write.table(prs.result, paste0("plots/",outname,"R2-table.txt"),row.names=F,col.names = T)
-# line = paste0(substr(name,length()))
 
-# 
-# # Plot PRS
-# 
-# # library(ggplot2)
-# # # generate a pretty format for p-value output
-# # prs.result$print.p <- round(prs.result$P, digits = 3)
-# # prs.result$print.p[!is.na(prs.result$print.p) &
-# #                      prs.result$print.p == 0] <-
-# #   format(prs.result$P[!is.na(prs.result$print.p) &
-# #                         prs.result$print.p == 0], digits = 2)
-# # prs.result$print.p <- sub("e", "*x*10^", prs.result$print.p)
-# # # Initialize ggplot, requiring the threshold as the x-axis (use factor so that it is uniformly distributed)
-# # ggplot(data = prs.result, aes(x = factor(Threshold), y = R2)) +
-# #   # Specify that we want to print p-value on top of the bars
-# #   geom_text(
-# #     aes(label = paste(print.p)),
-# #     vjust = -1.5,
-# #     hjust = 0,
-# #     angle = 45,
-# #     cex = 4,
-# #     parse = T
-# #   )  +
-# #   # Specify the range of the plot, *1.25 to provide enough space for the p-values
-# #   scale_y_continuous(limits = c(0, max(prs.result$R2) * 1.25)) +
-# #   # Specify the axis labels
-# #   xlab(expression(italic(P) - value ~ threshold ~ (italic(P)[T]))) +
-# #   ylab(expression(paste("PRS model fit:  ", R ^ 2))) +
-# #   # Draw a bar plot
-# #   geom_bar(aes(fill = -log10(P)), stat = "identity") +
-# #   # Specify the colors
-# #   scale_fill_gradient2(
-# #     low = "dodgerblue",
-# #     high = "firebrick",
-# #     mid = "dodgerblue",
-# #     midpoint = 1e-4,
-# #     name = bquote(atop(-log[10] ~ model, italic(P) - value),)
-# #   ) +
-# #   # Some beautification of the plot
-# #   theme_classic() + theme(
-# #     axis.title = element_text(face = "bold", size = 18),
-# #     axis.text = element_text(size = 14),
-# #     legend.title = element_text(face = "bold", size =
-# #                                   18),
-# #     legend.text = element_text(size = 14),
-# #     axis.text.x = element_text(angle = 45, hjust =
-# #                                  1)
-# #   )
-# # # save the plot
-# # ggsave(paste0("plots/",outname,"val-R2",".png"), height = 7, width = 7)
-# 
-# 
-# # # Read in the files
-# # for (t in p.threshold){
-# #   print(paste0("generating ",outname,t,"-val.profile"))
-# #   prs <- read.table(paste0(outname,t,"-val.profile"), header=F)
-# #   prs <- prs[,c(1,1,3)]
-# #   colnames(prs) <- c("FID","IID", "SCORE")
-# #   # Merge the files
-# #   dat <- merge(prs, phenotype)
-# #   
-# #   
-# #   # Start plotting
-# #   ggplot(dat, aes(x=SCORE, y=Phen),title)+
-# #     ggtitle(paste0("PRS phenotype predictions for ", outname,"threshold=",t))+
-# #     geom_point()+
-# #     theme_classic()+
-# #     labs(x="Polygenic Score", y="Phen")
-# #   
-# #   ggsave(paste0("../plots/",outname,t,"-val-PRS-plot.png"), height = 7, width = 7)
-# # }
